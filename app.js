561462a0cdf3cc41dce6ee1fcd51e96b21be1eb5
Andrew <andrew.yu@cirruscpq.com>
2014-08-02 16:19:36 -0700

    this is my first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var lights = require('./routes/lights');
+
+
+var Firebase = require("firebase");
+
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// Routes defined here 
+
+
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.get('/lights', lights.list);
+
+
+// var myFireBaseRef = new Firebase("ra.firebaseio.com");
+var myFireBaseRef = new Firebase("ra.firebaseio.com/devices/virtualbulb");
+
+myFireBaseRef.set({
+  title: "Hello World!",
+  author: "Andrew",
+  colour: {
+    H: 0,
+    S: 0,
+    V: 0,
+    A: 1,
+    On: false
+  }
+});
+
+// myFireBaseRef.child("alanisawesome").set({
+//   date_of_birth: "June 23, 1912",
+//   full_name: "Alan Turing"
+// });
+// myFireBaseRef.child("gracehop").set({
+//   date_of_birth: "December 9, 1906",
+//   full_name: "Grace Hopper"
+// });
+
+// // Updating
+
+// var hopperRef = myFireBaseRef.child("gracehop");
+// hopperRef.update({
+//   "nickname": "Amazing Grace"
+// });
+
+myFireBaseRef.child("colour").update({"H": 123123});
+
+
+myFireBaseRef.on('value', function(color){
+  console.log("I've been changed: ")
+  
+  var colorOutput = color.val();
+
+  console.log(colorOutput)
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/lights.js
@@ -0,0 +1,39 @@
+var Firebase = require("firebase");
+
+
+exports.list = function(req, res){
+  
+  // Read from virtualbulb
+
+  // Get a reference to our posts
+  var postsRef = new Firebase("ra.firebaseio.com/devices/virtualbulb");
+
+  // Attach an asynchronous callback to read the data at our posts reference
+  postsRef.on('value', function (lightData) {
+    
+    var lightData = lightData.val();
+
+    var author = lightData.author;
+
+    // Convert color (HSV to CSS styling)
+
+    res.render('lights', { 
+      title     : 'LET THERE BE LIGHT',
+      lightData : lightData,
+      author : author
+    });
+
+    console.log("This should be changing")
+    console.log(lightData)
+      
+
+  }, function (errorObject) {
+    console.log('The read failed: ' + errorObject.code);
+  });
+
+
+
+};
+
+exports.lightsctrl = 
+
